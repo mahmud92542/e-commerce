20ffebbf76c774d6fadc7e8cdc2d961de060a887
mahmud92542 <mhasan92542@gmail.com>
2019-07-16 00:07:26 +0600

    edithtml files

--- src/urls.py
+++ src/urls.py
@@ -3,10 +3,12 @@ from .views import *
 
 urlpatterns = [
 	path('',home, name='home'),
-	path('',home, name='home'),
-	path('',home, name='home'),
-	path('',home, name='home'),
-	path('',home, name='home'),
-	path('',home, name='home'),
-	path('',home, name='home'),
+	path('about/',about, name='about'),
+	path('checkout/',checkout, name='checkout'),
+	path('faqs/',faqs, name='faqs'),
+	path('help/',helps, name='help'),
+	path('payment/',payment, name='payment'),
+	path('privacy/',privacy, name='privacy'),
+	path('product/',product, name='product'),
+	path('contact/',contact, name='contact'),
 ]
--- src/views.py
+++ src/views.py
@@ -3,4 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-	return render (request,'src/index.html') 
+	return render (request,'src/index.html')
+
+def about(request):
+	return render (request,'src/about.html')
+
+def checkout(request):
+	return render (request,'src/checkout.html')
+
+def faqs(request):
+	return render (request,'src/faqs.html')
+
+def helps(request):
+	return render (request,'src/help.html')
+
+def payment(request):
+	return render (request,'src/payment.html')
+
+def privacy(request):
+	return render (request,'src/privacy.html')
+
+def product(request):
+	return render (request,'src/product.html')
+
+def contact(request):
+	return render (request,'src/contact.html')
+
+def helps(request):
+	return render (request,'src/help.html')
